10553d3528dc242617e21c315a3caa1f7f1021e7
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2020-12-08 17:12:34 +0300

    [TH2-1128] Refactored code * logging * warns in code

--- src/main/java/com/exactpro/th2/mstore/AbstractMessageStore.java
+++ src/main/java/com/exactpro/th2/mstore/AbstractMessageStore.java
@@ -14,12 +14,14 @@
 package com.exactpro.th2.mstore;
 
 import static com.exactpro.cradle.messages.StoredMessageBatch.MAX_MESSAGES_COUNT;
+import static com.google.protobuf.TextFormat.shortDebugString;
+import static org.apache.commons.lang3.builder.ToStringStyle.NO_CLASS_NAME_STYLE;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
@@ -32,7 +34,6 @@ import java.util.stream.Collectors;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.jetbrains.annotations.NotNull;
 
 import com.exactpro.cradle.CradleManager;
@@ -44,8 +45,9 @@ import com.exactpro.cradle.messages.StoredMessageId;
 import com.exactpro.cradle.utils.CradleStorageException;
 import com.exactpro.th2.common.schema.message.MessageRouter;
 import com.exactpro.th2.store.common.AbstractStorage;
+import com.google.protobuf.GeneratedMessageV3;
 
-public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
+public abstract class AbstractMessageStore<T extends GeneratedMessageV3, M extends GeneratedMessageV3> extends AbstractStorage<T> {
     private final Map<SessionKey, AtomicLong> sessionToLastSequence = new ConcurrentHashMap<>();
     private final Map<CompletableFuture<Void>, StoredMessageBatch> asyncStoreFutures = new ConcurrentHashMap<>();
 
@@ -54,12 +56,14 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
     }
 
     @Override
-    public final void handle(T delivery) {
+    public final void handle(T messageBatch) {
         try {
-            verifyDelivery(delivery);
-            List<M> messages = getMessages(delivery);
+            verifyBatch(messageBatch);
+            List<M> messages = getMessages(messageBatch);
             if (messages.isEmpty()) {
-                logger.warn("Empty batch has been received"); //FIXME: need identify
+                if (logger.isWarnEnabled()) {
+                    logger.warn("Empty batch has been received {}", shortDebugString(messageBatch));
+                }
                 return;
             }
             M lastMessage = messages.get(messages.size() - 1);
@@ -69,12 +73,12 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
             AtomicLong lastSeqHolder = sessionToLastSequence.computeIfAbsent(sessionKey, ignore -> new AtomicLong(Long.MIN_VALUE));
             long prevLastSeq = lastSeqHolder.getAndAccumulate(lastSequence, Math::max);
             if (prevLastSeq >= firstSequence) {
-                logger.error("Duplicated delivery found: {}", delivery);
+                logger.error("Duplicated delivery found: {}", messageBatch);
                 return;
             }
             storeMessages(messages);
         } catch (Exception ex) {
-            logger.error("Cannot handle delivery of type {}", delivery.getClass(), ex);
+            logger.error("Cannot handle delivery of type {}", messageBatch.getClass(), ex);
         }
     }
 
@@ -82,7 +86,8 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
     public void dispose() {
         super.dispose();
 
-        Set<CompletableFuture<Void>> futuresToRemove = new HashSet<>();
+        logger.debug("Waiting for futures completion");
+        Collection<CompletableFuture<Void>> futuresToRemove = new HashSet<>();
         while (!(asyncStoreFutures.isEmpty() || Thread.currentThread().isInterrupted())) {
             logger.info("Wait for the completion of {} futures", asyncStoreFutures.size());
             futuresToRemove.clear();
@@ -97,15 +102,15 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
                     Thread.currentThread().interrupt();
                 } catch (CancellationException e) {
                     if (logger.isWarnEnabled()) {
-                        logger.warn("{} - future related to {} batch is cancelled", getClass().getPackageName(), formatStoredMessageBatch(batch, false), e);
+                        logger.warn("{} - future related to {} batch is cancelled", getClass().getSimpleName(), formatStoredMessageBatch(batch, false), e);
                     }
                 } catch (ExecutionException e) {
                     if (logger.isWarnEnabled()) {
-                        logger.warn("{} - storing {} batch is failure", getClass().getPackageName(), formatStoredMessageBatch(batch, false), e);
+                        logger.warn("{} - storing {} batch is failure", getClass().getSimpleName(), formatStoredMessageBatch(batch, false), e);
                     }
                 } catch (TimeoutException e) {
                     if (logger.isErrorEnabled()) {
-                        logger.error("{} - future related to {} batch can't be complited", getClass().getPackageName(), formatStoredMessageBatch(batch, false), e);
+                        logger.error("{} - future related to {} batch can't be complited", getClass().getSimpleName(), formatStoredMessageBatch(batch, false), e);
                     }
                     future.cancel(false);
                 }
@@ -128,25 +133,25 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
             try {
                 if (exception == null) {
                     if (logger.isDebugEnabled()) {
-                        logger.debug("{} - batch stored: {}", getClass().getPackageName(), formatStoredMessageBatch(storedMessageBatch, true));
+                        logger.debug("{} - batch stored: {}", getClass().getSimpleName(), formatStoredMessageBatch(storedMessageBatch, true));
                     }
                 } else {
                     if (logger.isErrorEnabled()) {
-                        logger.error("{} - batch storing is failure: {}", getClass().getPackageName(), formatStoredMessageBatch(storedMessageBatch, true));
+                        logger.error("{} - batch storing is failure: {}", getClass().getSimpleName(), formatStoredMessageBatch(storedMessageBatch, true), exception);
                     }
                 }
             } finally {
                 if(asyncStoreFutures.remove(future) == null) {
                     if (logger.isWarnEnabled()) {
-                        logger.warn("{} - future related to batch {} already removed", getClass().getPackageName(), formatStoredMessageBatch(storedMessageBatch, true));
+                        logger.warn("{} - future related to batch {} already removed", getClass().getSimpleName(), formatStoredMessageBatch(storedMessageBatch, true));
                     }
                 }
             }
         });
     }
 
-    private String formatStoredMessageBatch(StoredMessageBatch storedMessageBatch, boolean full) {
-        ToStringBuilder builder = new ToStringBuilder(storedMessageBatch)
+    private static String formatStoredMessageBatch(StoredMessageBatch storedMessageBatch, boolean full) {
+        ToStringBuilder builder = new ToStringBuilder(storedMessageBatch, NO_CLASS_NAME_STYLE)
                 .append("stream", storedMessageBatch.getStreamName())
                 .append("direction", storedMessageBatch.getId().getDirection())
                 .append("batch id", storedMessageBatch.getId().getIndex());
@@ -166,7 +171,7 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
      * and that each message has sequence number greater than the previous one.
      * @param delivery the delivery received from router
      */
-    private void verifyDelivery(T delivery) {
+    private void verifyBatch(T delivery) {
         List<M> messages = getMessages(delivery);
         SessionKey lastKey = null;
         long lastSeq = Long.MIN_VALUE;
@@ -186,7 +191,7 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
 
     }
 
-    private void verifySequence(int messageIndex, long lastSeq, long currentSeq) {
+    private static void verifySequence(int messageIndex, long lastSeq, long currentSeq) {
         if (lastSeq >= currentSeq) {
             throw new IllegalArgumentException(
                     String.format(
@@ -197,7 +202,7 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
         }
     }
 
-    private void verifySession(int messageIndex, SessionKey lastKey, SessionKey sessionKey) {
+    private static void verifySession(int messageIndex, SessionKey lastKey, SessionKey sessionKey) {
         if (!lastKey.equals(sessionKey)) {
             throw new IllegalArgumentException(
                     String.format(
@@ -236,14 +241,16 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
         }
 
         @Override
-        public boolean equals(Object o) {
-            if (this == o)
+        public boolean equals(Object obj) {
+            if (this == obj) {
                 return true;
+            }
 
-            if (o == null || getClass() != o.getClass())
+            if (obj == null || getClass() != obj.getClass()) {
                 return false;
+            }
 
-            SessionKey that = (SessionKey)o;
+            SessionKey that = (SessionKey)obj;
 
             return new EqualsBuilder()
                     .append(streamName, that.streamName)
@@ -253,7 +260,7 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
 
         @Override
         public int hashCode() {
-            return new HashCodeBuilder(17, 37)
+            return new HashCodeBuilder()
                     .append(streamName)
                     .append(direction)
                     .toHashCode();
@@ -261,7 +268,7 @@ public abstract class AbstractMessageStore<T, M> extends AbstractStorage<T> {
 
         @Override
         public String toString() {
-            return new ToStringBuilder(this, ToStringStyle.NO_CLASS_NAME_STYLE)
+            return new ToStringBuilder(this, NO_CLASS_NAME_STYLE)
                     .append("streamName", streamName)
                     .append("direction", direction)
                     .toString();
